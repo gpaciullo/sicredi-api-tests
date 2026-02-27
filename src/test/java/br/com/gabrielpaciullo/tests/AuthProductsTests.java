e354ffcb0baff1fbcf30a9b4144a7ac232c59d1c
gpaciullo <oliver.gabriel@gmail.com>
2026-02-27 16:33:33 -0300

    chore: formatting and stability fixes

--- src/test/java/br/com/gabrielpaciullo/tests/AuthProductsTests.java
+++ src/test/java/br/com/gabrielpaciullo/tests/AuthProductsTests.java
@@ -4,7 +4,6 @@ import br.com.gabrielpaciullo.client.AuthClient;
 import br.com.gabrielpaciullo.client.ProductsClient;
 import br.com.gabrielpaciullo.config.BaseTest;
 import br.com.gabrielpaciullo.model.LoginRequest;
-import io.restassured.response.Response;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -23,15 +22,11 @@ public class AuthProductsTests extends BaseTest {
 
 	@Test
 	public void deveAcessarAuthProductsComToken() {
-		Response loginResponse = auth.login(new LoginRequest("emilys", "emilyspass")).then()
-				.statusCode(anyOf(is(200), is(201))).extract().response();
+		String username = System.getProperty("username", "emilys");
+		String password = System.getProperty("password", "emilyspass");
 
-		String token = loginResponse.path("accessToken");
-		if (token == null || token.isBlank())
-			token = loginResponse.path("token");
-
-		// garante que conseguiu pegar
-		org.testng.Assert.assertTrue(token != null && !token.isBlank(), "Token não retornado no login.");
+		String token = auth.login(new LoginRequest(username, password)).then().statusCode(anyOf(is(200), is(201)))
+				.body("accessToken", not(isEmptyOrNullString())).extract().path("accessToken");
 
 		products.authProducts(token).then().statusCode(200).body("products", is(not(empty())));
 	}
